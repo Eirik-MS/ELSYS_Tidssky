3a24c60e88f0a11fe50eb53bc4c149c6d3c6345a
Nik <nikolsn@stud.ntnu.no>
2022-11-03 14:44:13 -0700

    Ny fil

--- KnappArduinoStrip/KnappArduinoStrip.ino
+++ KnappArduinoStrip/KnappArduinoStrip.ino
@@ -1,62 +1,43 @@
 //Knapp Sketch
- #include <FastLED.h>
- // change these to match your hardware setup
- 
-#define LED_PIN 7                         // hardware SPI pin SCK
-#define NUM_LEDS 45
-#define LED_TYPE NEOPIXEL
-#define MIN_BRIGHTNESS 240                   // watch the power!
-#define MAX_BRIGHTNESS 255                   // watch the power!
-
-struct CRGB leds[NUM_LEDS];
-
-
-// constants won't change. They're used here to
-// set pin numbers:
-const int buttonPin = 2;     // the number of the pushbutton pin
-// variables will change:
-int buttonState = 0;         // variable for reading the pushbutton status
+#include <FastLED.h>  // Inkluderer biblioteket vi trenger
+#define LED_PIN  13                       // Setter pin for rgb-strip til 13
+#define NUM_LEDS 45 // Setter antall dioder på listen til 45
+#define LED_TYPE NEOPIXEL // Setter pixeltype
+#define MIN_BRIGHTNESS 240                   // Lysstyrke for lightstrip
+#define MAX_BRIGHTNESS 255        
+           
+struct CRGB leds[NUM_LEDS]; // CRGB skal endre antall LEDs
+const int buttonPin = 2;     // Setter buttonpin som en konstant 2
+int buttonState = 0;         // Setter startverdi til button som 0
 void setup() {
-  LEDS.addLeds<LED_TYPE, LED_PIN>(leds, NUM_LEDS);
-  FastLED.setBrightness(MAX_BRIGHTNESS);
-  FastLED.clear();
+  LEDS.addLeds<LED_TYPE, LED_PIN>(leds, NUM_LEDS); // Definerer antall leds
+  FastLED.setBrightness(MAX_BRIGHTNESS); // Setter lysstyrke
+  FastLED.clear(); // Tømmer led-strip
   Serial.begin(9600);
-  // initialize the LED pin as an output:
-  // initialize the pushbutton pin as an input:
-  pinMode(buttonPin, INPUT);
+  pinMode(buttonPin, INPUT); // Setter button som input
 }
 int hue = 0;
 int divisor = 30;
 void loop() {
-  // read the state of the pushbutton value:
   buttonState = digitalRead(buttonPin);
-  // Show the state of pushbutton on serial monitor
   Serial.println(buttonState);
-  // check if the pushbutton is pressed.
-  // if it is, the buttonState is HIGH:
-  if (buttonState == HIGH) {
-    float breath = (exp(sin(millis()/50.0*PI)) - 0.36787944)*108.0;
-    breath = map(breath, 0, 255, MIN_BRIGHTNESS, MAX_BRIGHTNESS);
-    FastLED.setBrightness(breath);
+  if (buttonState == HIGH) {                 // Kjører loopen dersom button er trykket inn
+    delay(100);
+    for(int dot = 0; dot < NUM_LEDS; dot++) { // For løkke som endrer en og en LED til fargen oppgitt
+      leds[dot] = CRGB(218,51,255); // Setter farge
+      FastLED.show(); // viser farge
+      delay(10); // senker ned hastigheten diodene endrer farge i 
+      } 
     delay(100);
     for(int dot = 0; dot < NUM_LEDS; dot++) {
-      leds[dot] = CRGB(218,51,255);
+      leds[dot] = CRGB(151,17,233);
       FastLED.show();
-      // clear this led for the next time around the loop
       delay(10);
       }
-      delay(100);
-    for(int dot = 0; dot < NUM_LEDS; dot++) { 
-       leds[dot] = CRGB(151,17,233);
-       FastLED.show();
-         // clear this led for the next time around the loop
-       delay(10);
-      }
-        delay(100);
+    delay(100);
     for(int dot = 0; dot < NUM_LEDS; dot++) {
       leds[dot] = CRGB(19,141,117);
       FastLED.show();
-            // clear this led for the next time around the loop
       delay(10);
           }
   
@@ -64,21 +45,18 @@ void loop() {
     for(int dot = 0; dot < NUM_LEDS; dot++) { 
             leds[dot] = CRGB(17,233,223);
             FastLED.show();
-            // clear this led for the next time around the loop
             delay(10);
         }
         delay(100);
     for(int dot = 0; dot < NUM_LEDS; dot++) { 
             leds[dot] = CRGB(218,51,255);
             FastLED.show();
-            // clear this led for the next time around the loop
             delay(10);
         }
         delay(100);
     for(int dot = 0; dot < NUM_LEDS; dot++) { 
           leds[dot] = CRGB(151,17,233);
           FastLED.show();
-          // clear this led for the next time around the loop
           delay(10);
         }
 
@@ -86,7 +64,6 @@ void loop() {
     for(int dot = 0; dot < NUM_LEDS; dot++) { 
           leds[dot] = CRGB(19,141,117);
           FastLED.show();
-          // clear this led for the next time around the loop
           delay(10);
           }
   
@@ -94,20 +71,17 @@ void loop() {
     for(int dot = 0; dot < NUM_LEDS; dot++) { 
             leds[dot] = CRGB(17,233,223);
             FastLED.show();
-            // clear this led for the next time around the loop
             delay(10);
         }
     for(int dot = 0; dot < NUM_LEDS; dot++) { 
             leds[dot] = CRGB(218,51,255);
             FastLED.show();
-            // clear this led for the next time around the loop
             delay(10);
         }
         delay(100);
     for(int dot = 0; dot < NUM_LEDS; dot++) { 
             leds[dot] = CRGB(151,17,233);
             FastLED.show();
-            // clear this led for the next time around the loop
             delay(10);
         }
 
@@ -115,7 +89,6 @@ void loop() {
     for(int dot = 0; dot < NUM_LEDS; dot++) { 
             leds[dot] = CRGB(19,141,117);
             FastLED.show();
-            // clear this led for the next time around the loop
             delay(10);
           }
   
@@ -123,21 +96,18 @@ void loop() {
     for(int dot = 0; dot < NUM_LEDS; dot++) { 
             leds[dot] = CRGB(17,233,223);
             FastLED.show();
-            // clear this led for the next time around the loop
             delay(10);
         }
         delay(100);
     for(int dot = 0; dot < NUM_LEDS; dot++) { 
             leds[dot] = CRGB(218,51,255);
             FastLED.show();
-            // clear this led for the next time around the loop
             delay(10);
         }
         delay(100);
     for(int dot = 0; dot < NUM_LEDS; dot++) { 
             leds[dot] = CRGB(151,17,233);
             FastLED.show();
-            // clear this led for the next time around the loop
             delay(10);
         }
 
@@ -145,7 +115,6 @@ void loop() {
     for(int dot = 0; dot < NUM_LEDS; dot++) { 
             leds[dot] = CRGB(19,141,117);
             FastLED.show();
-            // clear this led for the next time around the loop
             delay(10);
           }
   
@@ -153,7 +122,6 @@ void loop() {
     for(int dot = 0; dot < NUM_LEDS; dot++) { 
             leds[dot] = CRGB(17,233,223);
             FastLED.show();
-            // clear this led for the next time around the loop
             delay(10);
         }
 
@@ -161,21 +129,18 @@ void loop() {
     for(int dot = 0; dot < NUM_LEDS; dot++) { 
             leds[dot] = CRGB::Black;
             FastLED.show();
-            // clear this led for the next time around the loop
             delay(10);
         }
         delay(2000);
     for(int dot = 0; dot < NUM_LEDS; dot++) { 
             leds[dot] = CRGB(255,100,0);
             FastLED.show();
-            // clear this led for the next time around the loop
             delay(10);
         } delay(10);
         
     for(int dot = 0; dot < NUM_LEDS; dot++) { 
             leds[dot] = CRGB(255,128,0);
             FastLED.show();
-            // clear this led for the next time around the loop
             delay(10);
         }
 
@@ -184,7 +149,6 @@ void loop() {
     for(int dot = 0; dot < NUM_LEDS; dot++) { 
             leds[dot] = CRGB(202,102,0);
             FastLED.show();
-            // clear this led for the next time around the loop
             delay(10);
           }
   
@@ -192,21 +156,18 @@ void loop() {
     for(int dot = 0; dot < NUM_LEDS; dot++) { 
             leds[dot] = CRGB(255,120,0);
             FastLED.show();
-            // clear this led for the next time around the loop
             delay(10);
         }
         delay(2000);
     for(int dot = 0; dot < NUM_LEDS; dot++) { 
             leds[dot] = CRGB(255,100,0);
             FastLED.show();
-            // clear this led for the next time around the loop
             delay(10);
         } delay(10);
         
     for(int dot = 0; dot < NUM_LEDS; dot++) { 
             leds[dot] = CRGB(255,128,0);
             FastLED.show();
-            // clear this led for the next time around the loop
             delay(10);
         }
 
@@ -215,7 +176,6 @@ void loop() {
     for(int dot = 0; dot < NUM_LEDS; dot++) { 
             leds[dot] = CRGB(202,102,0);
             FastLED.show();
-            // clear this led for the next time around the loop
             delay(10);
           }
   
@@ -223,103 +183,88 @@ void loop() {
     for(int dot = 0; dot < NUM_LEDS; dot++) { 
             leds[dot] = CRGB(255,120,0);
             FastLED.show();
-            // clear this led for the next time around the loop
             delay(10);
         }
     for(int dot = 0; dot < NUM_LEDS; dot++) { 
-           leds[dot] = CRGB::Black;
+            leds[dot] = CRGB::Black;
             FastLED.show();
-            // clear this led for the next time around the loop
             delay(10);
         }
         delay(2000);
+        delay(100);
+    for(int dot = 0; dot < NUM_LEDS; dot++) { 
+            leds[dot] = CRGB::Black;
+            FastLED.show();
+            delay(10);
+        }
+        delay(100);
+    for(int dot = 0; dot < NUM_LEDS; dot++) { 
+            leds[dot] = CRGB(255,20,100);
+            FastLED.show();
+            delay(10);
+        } delay(10);
+        
+    for(int dot = 0; dot < NUM_LEDS; dot++) { 
+            leds[dot] = CRGB(245,0,138);
+            FastLED.show();
+            delay(10);
+        }
+
+
+        delay(100);
+    for(int dot = 0; dot < NUM_LEDS; dot++) { 
+            leds[dot] = CRGB(255,0,107);
+            FastLED.show();
+            delay(10);
+          }
+  
+        delay(100);
+    for(int dot = 0; dot < NUM_LEDS; dot++) { 
+            leds[dot] = CRGB(255,0,100);
+            FastLED.show();
+            delay(10);
+        }
+        delay(100);
+    for(int dot = 0; dot < NUM_LEDS; dot++) { 
+            leds[dot] = CRGB(255,20,100);
+            FastLED.show();
+            delay(10);
+        } delay(10);
+        
+    for(int dot = 0; dot < NUM_LEDS; dot++) { 
+            leds[dot] = CRGB(245,0,138);
+            FastLED.show();
+            delay(10);
+        }
+
+
+        delay(100);
+    for(int dot = 0; dot < NUM_LEDS; dot++) { 
+            leds[dot] = CRGB(255,0,107);
+            FastLED.show();
+            delay(10);
+          }
+  
+        delay(100);
+    for(int dot = 0; dot < NUM_LEDS; dot++) { 
+            leds[dot] = CRGB(255,0,100);
+            FastLED.show();
+            delay(10);
+        }
+    for(int dot = 0; dot < NUM_LEDS; dot++) { 
+            leds[dot] = CRGB::Black;
+            FastLED.show();
+            delay(10);
+            }
+        delay(2000);
+  } else {                                    // Else står i forhold til om knappen er trykket, så dersom knappen er 0, vil alle diodene endre til svart.
     delay(100);
     for(int dot = 0; dot < NUM_LEDS; dot++) { 
-            leds[dot] = CRGB::Black;
-            FastLED.show();
-            // clear this led for the next time around the loop
-            delay(10);
-        }
-        delay(100);
-    for(int dot = 0; dot < NUM_LEDS; dot++) { 
-            leds[dot] = CRGB(255,20,100);
-            FastLED.show();
-            // clear this led for the next time around the loop
-            delay(10);
-        } delay(10);
-        
-    for(int dot = 0; dot < NUM_LEDS; dot++) { 
-            leds[dot] = CRGB(245,0,138);
-            FastLED.show();
-            // clear this led for the next time around the loop
-            delay(10);
-        }
-
-
-        delay(100);
-    for(int dot = 0; dot < NUM_LEDS; dot++) { 
-            leds[dot] = CRGB(255,0,107);
-            FastLED.show();
-            // clear this led for the next time around the loop
-            delay(10);
-          }
-  
-        delay(100);
-    for(int dot = 0; dot < NUM_LEDS; dot++) { 
-            leds[dot] = CRGB(255,0,100);
-            FastLED.show();
-            // clear this led for the next time around the loop
-            delay(10);
-        }
-        delay(100);
-    for(int dot = 0; dot < NUM_LEDS; dot++) { 
-            leds[dot] = CRGB(255,20,100);
-            FastLED.show();
-            // clear this led for the next time around the loop
-            delay(10);
-        } delay(10);
-        
-    for(int dot = 0; dot < NUM_LEDS; dot++) { 
-            leds[dot] = CRGB(245,0,138);
-            FastLED.show();
-            // clear this led for the next time around the loop
-            delay(10);
-        }
-
-
-        delay(100);
-    for(int dot = 0; dot < NUM_LEDS; dot++) { 
-            leds[dot] = CRGB(255,0,107);
-            FastLED.show();
-            // clear this led for the next time around the loop
-            delay(10);
-          }
-  
-        delay(100);
-    for(int dot = 0; dot < NUM_LEDS; dot++) { 
-            leds[dot] = CRGB(255,0,100);
-            FastLED.show();
-            // clear this led for the next time around the loop
-            delay(10);
-        }
-    for(int dot = 0; dot < NUM_LEDS; dot++) { 
-            leds[dot] = CRGB::Black;
-            FastLED.show();
-            // clear this led for the next time around the loop
-            delay(10);
-        }
-        delay(2000);
-
-  } else {
-    // turn LED off:
-      delay(100);
-      for(int dot = 0; dot < NUM_LEDS; dot++) { 
             leds[dot] = CRGB(0,0,0);
             FastLED.show();
-            // clear this led for the next time around the loop
             delay(10);
-        }
+            }
   }
-  // Added the delay so that we can see the output of button
-  delay(100);
+     
+
 }
